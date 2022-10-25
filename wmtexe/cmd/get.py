9f6e5a20e65765389682161b985cab186db88fce
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-10-25 18:57:21 -0500

    Adding tarfile member sanitization to extractall() (#19)

--- wmtexe/cmd/get.py
+++ wmtexe/cmd/get.py
@@ -29,7 +29,26 @@ def download_or_exit(url, id, dest):
 def unpack_or_exit(name, dest):
     try:
         with tarfile.open(name, 'r') as tar:
-            tar.extractall(path=dest)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=dest)
     except tarfile.TarError as error:
         print('==> Error: %s' % error)
         sys.exit(1)
